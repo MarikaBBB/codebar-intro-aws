a9d6bb56dabc38af5096b2ff597b44b9de96a13e
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-10-29 18:15:35 +0000

    update GetItemsFunctiion

--- src/GetItemsFunction/handler.py
+++ src/GetItemsFunction/handler.py
@@ -1,12 +1,17 @@
 import json
+import os
+import boto3
+import uuid
+
+TABLE_NAME = os.environ["ITEMSTABLE_TABLE_NAME"]
+
+ddb = boto3.resource("dynamodb")
+table = ddb.Table(TABLE_NAME)
 
 def handler(event, context):
-    # Log the event argument for debugging and for use in local development.
-    print(json.dumps(event))
+    response = table.scan()
 
     return {
         "statusCode": 200,
-        "body": json.dumps([
-            {"id": "1", "title": "Attend codebar talk"}
-        ])
+        "body": json.dumps(response["Items"])
     }
